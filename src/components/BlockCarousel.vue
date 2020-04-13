358de240c899711417aef28347e9fb3f5bf3feea
Lidiya Larchenko <l.larchenko@anketolog.ru>
2020-04-13 18:20:08 +0700

    slick

--- src/components/BlockCarousel.vue
+++ src/components/BlockCarousel.vue
@@ -5,38 +5,12 @@
         <h1>{{ block.title }}</h1>
         <p>{{ block.text }}</p>
 
-
-        <div id="carousel" ref="carousel" class="carousel slide carousel-multi-item" data-ride="carousel">
-
-            <div id="carousel-inner" class="carousel-inner" role="listbox">
-                <div :class="'carousel-item ' + (slide.isActive ? 'active' : '')"
-                     v-for="slide of block.slides" :key="slide.id">
-                    <img :src="slide.picture" class="d-block w-100" :alt="slide.id">
-                </div>
-            </div>
-
-            <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-
-            <ol class="carousel-indicators">
-                <li :class="{active: slide.isActive}"
-                    v-for="slide of block.slides" :key="slide.id"
-                    data-target="#carousel" :data-slide-to="slide.id-1"></li>
-            </ol>
-        </div>
-
-
-        <div v-if="block.picture" class="background-image"></div>
-
-
         <slick ref="slick" :options="slickOptions">
 
+            <div :class="'slide-item ' + (slide.isActive ? 'active' : '')"
+                 v-for="slide of block.slides" :key="slide.id">
+                <img :src="slide.picture" :alt="slide.id">
+            </div>
             <div :class="'slide-item ' + (slide.isActive ? 'active' : '')"
                  v-for="slide of block.slides" :key="slide.id">
                 <img :src="slide.picture" :alt="slide.id">
@@ -44,6 +18,16 @@
 
         </slick>
 
+        <a class="slick-prev" v-on:click="prev"></a>
+        <a class="slick-next" v-on:click="next"></a>
+
+        <ol class="carousel-indicators slick-dots">
+            <li :class="{active: slide.isActive}"
+                v-for="slide of block.slides" :key="slide.id"
+                ></li>
+        </ol>
+
+        <div v-if="block.picture" class="background-image"></div>
 
         <button v-if="block.button" class="btn btn-danger">
             <a v-if="block.button.url" :href="block.button.url" target="_blank">
@@ -55,7 +39,7 @@
 </template>
 
 <script>
-    // import $ from 'jquery';
+    import $ from 'jquery';
     import Slick from 'vue-slick';
 
     export default {
@@ -68,20 +52,23 @@
         data() {
             return {
                 slickOptions: {
-                    slidesToShow: 2,
+                    slidesToShow: 3,
                     centerMode: true,
-                    centerPadding: '60px',
-                    autoplay: true,
+                    centerPadding: '0px',
+                    //autoplay: true,
                     autoplaySpeed: 3000,
                     dots: true,
                     infinite: true,
                     //variableWidth: true,
+                    prevArrow: $(".slick-prev"),
+                    nextArrow: $(".slick-next"),
+                    appendDots: $(".slick-dots"),
                     responsive: [
                         {
                             breakpoint: 768,
                             settings: {
                                 centerMode: true,
-                                centerPadding: '60px',
+                                centerPadding: '0px',
                             }
                         },
                         {
@@ -96,7 +83,13 @@
             };
         },
 
-        mounted() {
+        methods: {
+            next() {
+                this.$refs.slick.next();
+            },
+            prev() {
+                this.$refs.slick.prev();
+            },
 
         }
     }
